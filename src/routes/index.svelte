9fc3412428aa3a405fda67ea693fe6115af5e31d
Albin Frick <abbzez@gmail.com>
2021-10-04 13:48:05 +0200

    init commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,26 @@
+<header><span>logga</span><ul>Menu</ul></header>
+<h1>Akvilinas</h1>
+<p>Uthyrning och konst</p>
+
+<style lang="scss">
+	header{
+		width: 100%;
+		height: 4rem;
+		display: flex;
+		justify-content: space-between;
+		span{
+			height: 100%;
+			background-color: red;
+		}
+	}
+	h1{
+		font-size: 6.25rem;
+		text-align: center;
+	}
+	p{
+		font-size: 1.875rem;
+		text-align: center;
+
+	}
+
+</style>
