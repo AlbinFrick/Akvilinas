84687fbb7674a5f5f838c09033593156cc0e2e87
Albin Frick <abbzez@gmail.com>
2021-10-17 00:41:15 +0200

    Updates MobileMenu

--- src/components/Menu/MobileMenu.svelte
+++ src/components/Menu/MobileMenu.svelte
@@ -4,7 +4,7 @@
 
 <nav class="sm:hidden bg-red-300 z-10 fixed bottom-4 w-full px-4 sm:px-8 ">
   <ul class="flex items-center justify-around  bg-accent py-2 px-4 rounded-3xl h-16">
-    <MenuItem url="/portfolio" text="Portfolio" />
+    <MenuItem url="/" text="Hem" />
     <MenuItem url="/hyra" text="Hyra" />
     <MenuItem url="/kontakt" text="Kontakt" />
     <MenuItem url="/om_akvilinas" text="Akvilinas" />
