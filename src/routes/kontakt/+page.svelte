0c9afcfdbe7342460c481cc9f81581b3b365feb1
AlbinFrick <abbzez@gmail.com>
2024-01-06 00:02:33 +0100

    fix: remove kontakt page

--- src/routes/kontakt/+page.svelte
+++
@@ -1,9 +0,0 @@
-<div class="px-6">
-	<h1 class="text-4xl md:text-6xl mt-10 font-bold tracking-tight text-gray-900 sm:text-6xl">
-		Kontakta mig!
-	</h1>
-	<p class="mt-6 text-lg md:text-2xl leading-8 text-gray-600 max-w-3xl">
-		Vill du beställa något från utbudet, lägga en specialbeställning eller ställa en fråga? Det gör
-		du enkelt här nedanför i formuläret.
-	</p>
-</div>
