9aed776b6c8d5e2b72eb7e1667a52b56a6299926
AlbinFrick <abbzez@gmail.com>
2024-01-06 00:54:20 +0100

    Refactor component props

--- src/lib/components/Text.svelte
+++ src/lib/components/Text.svelte
@@ -1,9 +1,10 @@
 <script lang='ts'>
 	import { cn } from "$lib/utils";
 
-	export let style: string;
+	export let className: string;
+	export { className as class };
 </script>
 
-<p class={cn('font-arapey text-lg', style)}>
+<p class={cn('font-arapey text-lg', className)}>
 	<slot />
 </p>
