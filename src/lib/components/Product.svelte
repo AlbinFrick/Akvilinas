5d063dbe1fe78fc86e1b3d30f0fb7a236e379da0
AlbinFrick <abbzez@gmail.com>
2024-01-06 01:44:18 +0100

    feat: adds correct information in the dialog

--- src/lib/components/Product.svelte
+++ src/lib/components/Product.svelte
@@ -4,13 +4,20 @@
 	import { Badge } from './ui/badge';
 	import  Text from './Text.svelte';
   import * as Dialog from "$lib/components/ui/dialog";
+  import { capitalizeFirstLetter } from '$lib/utils'; 
 
   export let product: Product;
+
+
+  function formatContents(array: string[] ){
+    return array.map(item => item.trim()).join(', ')
+  }
+
 </script>
 <Dialog.Root>
 <Dialog.Trigger>
 <div
-  class="flex shadow flex-col justify-between gap-6 bg-white rounded-2xl pb-6"
+  class="flex shadow flex-col justify-between gap-6 bg-white rounded-2xl pb-6 h-full"
 >
   <div class="flex flex-col gap-4">
     <div class="relative">
@@ -22,14 +29,14 @@
       {#if product.allergens && product.allergens.length > 0}
         <div class="absolute right-3 bottom-3 flex gap-4">
           {#each product.allergens as allergen}
-            <Badge variant="secondary" >{allergen}</Badge>
+            <Badge variant="secondary" >{capitalizeFirstLetter(allergen)}</Badge>
           {/each}
         </div>
       {/if}
     </div>
-    <div class="px-6 prose-h3:mt-3 prose-h3:mb-1 ">
+    <div class="px-6 text-start ">
       <h2>{product.name}</h2>
-      <p>
+      <Text class="pb-3 pt-2 text-2xl" >
         {#each product.price as price, index}
           <span class="font-bold"
             >{price}
@@ -39,17 +46,18 @@
           >
         {/each}
         kr
-      </p>
-      <h3 class="text-lg">Beskrivning:</h3>	
+      </Text>
       <Text class='line-clamp-3'>{product.description}</Text>
     </div>
   </div>
 </div>
-
 </Dialog.Trigger>
-  <Dialog.Content>
-      <h2>{product.name}</h2>
-      <p>
+  <Dialog.Content class='grid grid-cols-[3fr_2fr] max-w-4xl prose prose-h2:font-playfair prose-h2:mt-0 prose-h2:mb-0 prose-h3:mt-0 prose-h3:mb-0 prose-h3:font-playfair '>
+    <div>
+      <h2 class="text-4xl pb-3">{product.name}</h2>
+      <Text class='line-clamp-3'>{product.description}</Text>
+      <h3 class="text-lg">Pris:</h3>
+      <Text class='text-2xl font-bold'>
         {#each product.price as price, index}
           <span class="font-bold"
             >{price}
@@ -59,8 +67,17 @@
           >
         {/each}
         kr
-      </p>
-      <h3 class="text-lg">Beskrivning:</h3>	
-      <Text class='line-clamp-3'>{product.description}</Text>
+      </Text>
+
+      {#if product.contents}
+        <h3 class="text-lg">Innehåll:</h3>	
+        <Text>{formatContents(product.contents)}</Text>
+      {/if}
+      </div>
+      <img
+      class="rounded-xl w-full aspect-video md:aspect-[4/3] object-cover"
+      src={urlFor(product.image).url()}
+      alt={product.name}
+    />
   </Dialog.Content>
 </Dialog.Root>
