e3648ee37ef14439e50c1ffe98fa6fa11b4c878f
AlbinFrick <abbzez@gmail.com>
2024-01-06 04:09:54 +0100

    feat: add lightbox to images in image gallery

--- src/routes/galleri/+page.svelte
+++ src/routes/galleri/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { urlFor } from '$lib/sanity';
+	import { Lightbox } from 'svelte-lightbox'
 
 	/** @type {import('./$types').PageData} */
 	export let data;
@@ -8,47 +9,76 @@
 <div class="flex flex-col gap-4 lg:gap-9 not-prose my-4 md:my-12 px-4">
 	{#each data.gallery as gallery}
 		<div class="flex flex-col lg:flex-row gap-4 lg:gap-9">
-			<div class="flex flex-col-reverse lg:flex-col gap-4 lg:gap-9 flex-1">
-				<div class="flex gap-4 lg:gap-9">
-					<div class="rounded-xl overflow-hidden w-full h-72 lg:h-[450px]">
+			<div class="grid grid-cols-1 grid-rows-[288px_288px] md:grid-rows-[450px_622px] lg:flex-col gap-4 lg:gap-9 flex-1">
+				<div class="flex gap-4 lg:gap-9 w-full justify-center">
+    			<Lightbox transitionDuration={150} >
 						<img
-							class="object-cover h-full w-full"
+							slot="thumbnail"
+							class="object-cover rounded-xl w-full"
 							src={urlFor(gallery.portraitImageLeft).height(450).width(288).url()}
 							alt="images"
 						/>
-					</div>
-					<div class="rounded-xl overflow-hidden w-full h-72 lg:h-[450px]">
 						<img
-							class="object-cover h-full w-full"
+							class="object-cover rounded-xl px-4"
+							src={urlFor(gallery.portraitImageLeft).url()}
+							alt="images"
+						/>
+					</Lightbox>
+    			<Lightbox transitionDuration={150} >
+						<img
+							slot="thumbnail"
+							class="object-cover rounded-xl w-full"
 							src={urlFor(gallery.portraitImageRight).height(450).width(288).url()}
 							alt="images"
 						/>
-					</div>
+						<img
+							class="object-cover rounded-xl px-4"
+							src={urlFor(gallery.portraitImageRight).url()}
+							alt="images"
+						/>
+					</Lightbox>
 				</div>
-				<div class="rounded-xl overflow-hidden w-full h-72 lg:h-[622px]">
-					<img
-						class="object-cover h-full w-full"
-						src={urlFor(gallery.squareImage).height(622).width(622).url()}
-						alt="images"
-					/>
-				</div>
+    			<Lightbox transitionDuration={150} >
+						<img
+							slot="thumbnail"
+							class="object-cover rounded-xl w-full"
+							src={urlFor(gallery.squareImage).height(622).width(622).url()}
+							alt="images"
+						/>
+						<img
+							class="object-cover rounded-xl px-4"
+							src={urlFor(gallery.squareImage).url()}
+							alt="images"
+						/>
+					</Lightbox>
 			</div>
-			<div class="flex flex-col gap-4 lg:gap-9 flex-1">
-				<div class="rounded-xl overflow-hidden w-full h-96 lg:h-[764px]">
+			<div class="grid grid-cols-1 grid-rows-[384px_208px] md:grid-rows-[764px_308px] gap-4 lg:gap-9 flex-1 ">
+    		<Lightbox  transitionDuration={150} >
 					<img
-						class="object-cover h-full w-full"
+						slot="thumbnail"
+						class="object-cover rounded-xl w-full"
 						src={urlFor(gallery.bigPortraitImage).height(622).width(764).url()}
 						alt="images"
 					/>
-				</div>
-
-				<div class="rounded-xl overflow-hidden w-full h-52 lg:h-[308px]">
 					<img
-						class="object-cover h-full w-full"
+						class="object-cover rounded-xl px-4"
+						src={urlFor(gallery.bigPortraitImage).url()}
+						alt="images"
+					/>
+				</Lightbox>
+ 				<Lightbox transitionDuration={150} >
+					<img
+						slot="thumbnail"
+						class="object-cover rounded-xl w-full"
 						src={urlFor(gallery.landscapeImage).width(622).height(308).url()}
 						alt="images"
 					/>
-				</div>
+					<img
+						class="object-cover rounded-xl px-4"
+						src={urlFor(gallery.landscapeImage).url()}
+						alt="images"
+					/>
+				</Lightbox>
 			</div>
 		</div>
 	{/each}
