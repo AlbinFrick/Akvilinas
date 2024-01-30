93fc907ae5814c6935859ffa0994d2f76c6960fa
AlbinFrick <abbzez@gmail.com>
2024-01-30 21:12:47 +0100

    revert to old image layout

--- src/routes/galleri/+page.svelte
+++ src/routes/galleri/+page.svelte
@@ -8,40 +8,47 @@
 <div class="not-prose my-4 flex flex-col gap-4 px-4 md:my-12 lg:gap-9">
 	{#each data.gallery as gallery}
 		<div class="flex flex-col gap-4 lg:flex-row lg:gap-9">
-			<div
-				class="grid flex-1 grid-cols-1 grid-rows-[288px_288px] gap-4 md:grid-rows-[450px_622px] lg:flex-col lg:gap-9"
-			>
-				<div class="flex w-full justify-center gap-4 lg:gap-9">
+			<div class="flex flex-1 flex-col-reverse gap-4 lg:flex-col lg:gap-9">
+				<div class="flex gap-4 lg:gap-9">
+					<div class="h-72 w-full overflow-hidden rounded-xl lg:h-[450px]">
+						<img
+							class="h-full w-full object-cover"
+							src={urlFor(gallery.portraitImageLeft).height(450).width(288).url()}
+							alt="images"
+						/>
+					</div>
+					<div class="h-72 w-full overflow-hidden rounded-xl lg:h-[450px]">
+						<img
+							class="h-full w-full object-cover"
+							src={urlFor(gallery.portraitImageRight).height(450).width(288).url()}
+							alt="images"
+						/>
+					</div>
+				</div>
+				<div class="h-72 w-full overflow-hidden rounded-xl lg:h-[622px]">
 					<img
-						class="rounded-xl object-cover px-4"
-						src={urlFor(gallery.portraitImageLeft).height(450).width(288).url()}
-						alt="images"
-					/>
-					<img
-						class="rounded-xl object-cover px-4"
-						src={urlFor(gallery.portraitImageRight).height(450).width(288).url()}
+						class="h-full w-full object-cover"
+						src={urlFor(gallery.squareImage).height(622).width(622).url()}
 						alt="images"
 					/>
 				</div>
-				<img
-					class="rounded-xl object-cover px-4"
-					src={urlFor(gallery.squareImage).height(622).width(622).url()}
-					alt="images"
-				/>
 			</div>
-			<div
-				class="grid flex-1 grid-cols-1 grid-rows-[384px_208px] gap-4 md:grid-rows-[764px_308px] lg:gap-9"
-			>
-				<img
-					class="rounded-xl object-cover px-4"
-					src={urlFor(gallery.bigPortraitImage).height(622).width(764).url()}
-					alt="images"
-				/>
-				<img
-					class="rounded-xl object-cover px-4"
-					src={urlFor(gallery.landscapeImage).width(622).height(308).url()}
-					alt="images"
-				/>
+			<div class="flex flex-1 flex-col gap-4 lg:gap-9">
+				<div class="h-96 w-full overflow-hidden rounded-xl lg:h-[764px]">
+					<img
+						class="h-full w-full object-cover"
+						src={urlFor(gallery.bigPortraitImage).height(622).width(764).url()}
+						alt="images"
+					/>
+				</div>
+
+				<div class="h-52 w-full overflow-hidden rounded-xl lg:h-[308px]">
+					<img
+						class="h-full w-full object-cover"
+						src={urlFor(gallery.landscapeImage).width(622).height(308).url()}
+						alt="images"
+					/>
+				</div>
 			</div>
 		</div>
 	{/each}
