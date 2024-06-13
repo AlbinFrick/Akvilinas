40bcd99e031e236049ad30091064b030c66f187d
Albin Frick <abbzez@gmail.com>
2024-06-13 14:19:28 +0200

    update copy

--- src/lib/components/Product.svelte
+++ src/lib/components/Product.svelte
@@ -22,7 +22,7 @@
 		<p
 			class="absolute left-1/2 top-1/3 z-10 inline -translate-x-1/2 -translate-y-1/2 -rotate-[20deg] whitespace-nowrap rounded-md bg-primary px-3 py-2 text-xl text-primary-foreground"
 		>
-			Går inte att beställa just nu
+			Går ej att beställa för tillfället
 		</p>
 		<div class="opacity-40 sm:absolute sm:bottom-0 sm:top-0">
 			<ProductCard {product} />
