2734185f4adaa25c8737102c6e3aa02ca5bcde1d
Albin Frick <abbzez@gmail.com>
2024-06-18 00:33:02 +0200

    update copy for startpage, products, about

--- src/routes/om-akvilinas/+page.svelte
+++ src/routes/om-akvilinas/+page.svelte
@@ -7,10 +7,7 @@
 	<div class="mt-6 snap-y">
 		<h1 class="mb-3 scroll-m-20 text-4xl font-medium tracking-tight lg:text-5xl">Hilda Akvilina</h1>
 		<Text>
-			Mitt namn är Hilda och det är jag som driver Akvilinas Fika. Akvilinas Fika är en tårtverkstad
-			i centrala Lycksele. Vi skapar tårtor till alla tillfällen - allt från enkla prinsesstårtor
-			med modern design till mer avancerade tematårtor. Här kan du även finna godbitar, cheesecakes,
-			macarones och annat gott.
+			Mitt namn är Hilda och det är jag som driver Akvilinas Fika. Akvilinas Fika är en tårtstudio i centrala Lycksele. Vi skapar specialgjorda tematårtor till alla tillfällen.
 		</Text>
 		<Divider class="my-3 w-12 md:my-4" />
 		<Text>
@@ -22,6 +19,9 @@
 			resultat! Allt jag bakar gör jag naturligtvis utan gluten. Med en stor drivkraft att alla,
 			glutenintolerant eller icket, ska tycka att det är gott.
 		</Text>
+    <Text>
+      Under en lång tid har jag drömt om något eget, så under min mammaledighet hösten 2023 startade jag äntligen upp Akvilinas Fika.
+    </Text>
 		<Divider class="my-3 w-12 md:my-4" />
 		<Text>Kul att du har hittat hit! Välkommen med din beställning!</Text>
 		<Text>/Hilda</Text>
