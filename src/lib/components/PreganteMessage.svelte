07c1561bd2eea624a50c390822fd25cfd528de07
Albin Frick <abbzez@gmail.com>
2024-10-11 00:11:21 +0200

    emojis

--- src/lib/components/PreganteMessage.svelte
+++ src/lib/components/PreganteMessage.svelte
@@ -6,13 +6,13 @@
 </script>
 
 <span>
-	Hej kära besökare!
+	Hej kära besökare! 👋
 	<br />
 	Jag håller nu på att baka på en liten bebis istället för tårtor, därför har jag pausat alla beställningar
 	på obestämd tid. Kolla gärna på hemsidan och tänk ut något ni vill ha så hoppas jag att jag kan fylla
 	era beställningar när den lille är ute!
 	<br />
-	/Hilda
+	<span class="italic">/Hilda</span>
 	<button class="absolute right-2 top-2" on:click={() => toast_.dismiss(toast.id)}><XIcon /></button
 	>
 </span>
