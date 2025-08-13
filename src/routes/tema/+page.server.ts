2fbb0ec48894f14d0aab30bdb931474cc926ed27
Albin Frick <albinfrick@proton.me>
2025-08-13 21:51:19 +0200

    redirects users from tema to startpage

--- src/routes/tema/+page.server.ts
+++ src/routes/tema/+page.server.ts
@@ -1,7 +1,9 @@
 import { getThemePage } from '$lib/sanity';
+import { redirect } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
+	throw redirect(303, '/');
 	return {
 		page: await getThemePage()
 	};
